7e4a1375d9588b01eda395a2ac77ad24c8a120dc
theharshit2202 <109791319+theharshit2202@users.noreply.github.com>
2023-01-16 19:11:58 +0530

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,16 @@
+/*!
+ * @mainpage
+ *  @brief     Akinator
+ *  @author    Harshit Gupta and Manya Verma
+ *  @author    Harshit Gupta and Manya Verma
+ *  @version   C++23
+ *  @date      15.01.2023
+ *  @pre       First initialize the system.
+ *  @bug       No
+ *  @warning   Be careful! Limited size of string is 31 chars on default.
+ *  @copyright GNU Public License.
+ */
+
+#include "src/Interface.h"
+
+int main() { return Interface(); }
